0c30a3678b0810b4d61439057a09fe2be0889ab4
ChangXXX <eqqt97@naver.com>
2022-05-04 20:56:02 +0900

    chore: 초기 라이브러리 설정

--- app/src/main/java/com/todo/app/ui/MainActivity.kt
+++ app/src/main/java/com/todo/app/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.todo.app
+package com.todo.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.todo.app.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
