2f3d1dbe988827c84bba6a5fbcd94efc462f4f03
ChangXXX <eqqt97@naver.com>
2022-05-09 17:08:24 +0900

    feat: HomeFragment ui 구현

--- app/src/main/java/com/todo/app/ui/MainActivity.kt
+++ app/src/main/java/com/todo/app/ui/MainActivity.kt
@@ -2,13 +2,20 @@ package com.todo.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.todo.app.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_main)
+        navController = navHostFragment!!.findNavController()
     }
 }
